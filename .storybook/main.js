13348d5cc8198748a15fb2598499e347b1897b60
jeongnaehyeok <cnh0301@gmail.com>
2020-11-22 20:15:16 +0900

    [ Setup ] Storybook 설정 완료

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,22 @@
 const path = require('path');
 module.exports = {
-  stories: ['../src/stories/*.stories.js'],
+  stories: ['../src/stories/**/*.stories.js'],
   addons: ['@storybook/addon-actions', '@storybook/preset-create-react-app'],
   webpackFinal: async config => {
     config.module.rules = [
       {
+        test: /\.js$/,
+        include: path.resolve(__dirname, '../'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react'],
+          },
+        },
+      },
+      {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader'],
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
         include: path.resolve(__dirname, '../'),
       },
     ];
